5c85f87ec3f313d00a7e0ba742a3eb144dfacefe
user01 <sachin.saraswat1741@gmail.com>
2019-01-05 13:33:19 -0500

    Commit #05

---
+++ src/main/java/com/springinaction/tacocloud/controllers/DesignTacoController.java
@@ -0,0 +1,67 @@
+package com.springinaction.tacocloud.controllers;
+
+
+import com.springinaction.tacocloud.model.Ingredient;
+import com.springinaction.tacocloud.model.Ingredient.Type;
+import com.springinaction.tacocloud.model.Taco;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+
+    @GetMapping
+    public String showTacoDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Grounded peanuts Paste", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
+        Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+        model.addAttribute("design", new Taco());
+        return "tacoDesignForm";
+    }
+
+
+
+    @PostMapping
+    public String processDesign(Taco design) {
+// Save the taco design...
+// We'll do this in chapter 3
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
+    }
+
+    private Object filterByType(List<Ingredient> ingredients, Type type) {
+
+        List<Ingredient> returnList = new ArrayList<>();
+
+        ingredients.forEach((k)->{
+                if(k.getType().equals(type)) { returnList.add(k);}        });
+
+        return  returnList;
+
+    }
+}
---
+++ src/main/java/com/springinaction/tacocloud/model/Taco.java
@@ -0,0 +1,10 @@
+package com.springinaction.tacocloud.model;
+
+import lombok.Data;
+
+@Data
+public class Taco {
+
+    String name;
+
+}
