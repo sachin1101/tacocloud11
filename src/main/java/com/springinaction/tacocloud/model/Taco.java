561afb1b680346c6d9275c5ed4e9d9968ef8498a
user01 <sachin.saraswat1741@gmail.com>
2019-01-05 17:34:32 -0500

    Commit #06

--- src/main/java/com/springinaction/tacocloud/model/Taco.java
+++ src/main/java/com/springinaction/tacocloud/model/Taco.java
@@ -2,12 +2,18 @@ package com.springinaction.tacocloud.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
 
+     @Size(min=1, message="You must choose at least 1 ingredient")
      List<String> ingredientList;
+
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
     String name;
 
 }
